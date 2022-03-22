d63ccea24b99cfc19995182e6fb7e643443daff8
Nick Viatick <>
2022-03-22 17:35:22 +0700

    Update code

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-scp -r .next nickln@103.162.15.104:/home/nickln/elemon/gamefi-utilities-app/
-scp sitemap.xml nickln@103.162.15.104:/home/nickln/elemon/gamefi-utilities-app/
-scp package.json nickln@103.162.15.104:/home/nickln/elemon/gamefi-utilities-app/
-scp yarn.lock nickln@103.162.15.104:/home/nickln/elemon/gamefi-utilities-app/
+scp -r .next ubuntu@15.235.146.167:/home/ubuntu/elemon/crypto-game-tool-app/
+scp sitemap.xml ubuntu@15.235.146.167:/home/ubuntu/elemon/crypto-game-tool-app/
+scp package.json ubuntu@15.235.146.167:/home/ubuntu/elemon/crypto-game-tool-app/
+scp yarn.lock ubuntu@15.235.146.167:/home/ubuntu/elemon/crypto-game-tool-app/
